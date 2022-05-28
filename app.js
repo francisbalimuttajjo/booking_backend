ef434f4476b4883e0c58c17e64f7d291215a9267
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-05-28 23:31:09 +0300

    jjd

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 const cors = require("cors");
 const path = require("path");
+const helmet = require("helmet");
 const express = require("express");
+const { useTreblle } = require("treblle");
+const cookieParser = require("cookie-parser");
 const { sendResponse } = require("./utils/utils");
 const hotelRouter = require("./routes/hotel");
 const userRouter = require("./routes/user");
 const reviewRouter = require("./routes/review");
 const bookingRouter = require("./routes/booking");
 
+
+
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
 
-// app.use(helmet());
-//passing body
+
+
 app.use(express.json({ limit: "10kb" }));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+useTreblle(app, {
+  apiKey: "GvXnXxtKU4nQhSiRPaMK6ZPgO3QaNxiy",
+  projectId: "_YOUR_PROJECT_ID_",
+});
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 // app.use(express.static(path.join(__dirname, "public")));
 
-// Data sanitization against NoSQL query injection
-// app.use(mongoSanitize());
-// app.use(cookieParser());
 
-// // Data sanitization against XSS like malicious html
-// app.use(xss());
+
+
+
+
 
 app.get("/", (req, res) => {
   res.render("passwordReset.pug", { firstName: "bafra" });
@@ -37,8 +48,8 @@ app.use("/api/v1", userRouter);
 app.use("/api/v1", reviewRouter);
 app.use("/api/v1", bookingRouter);
 
-//not found route
 
+//not found route
 app.use("*", (req, res) =>
   sendResponse(
     req,
