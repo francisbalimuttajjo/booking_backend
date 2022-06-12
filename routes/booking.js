2407b42035235587f3eb38563218b7166734d1da
francis balimuttajjo <fbalimuttajjo@gmail.com>
2022-06-12 10:29:42 +0300

    included hotel on querying booking

--- routes/booking.js
+++ routes/booking.js
@@ -3,15 +3,13 @@ const bookingController = require("../controllers/booking");
 const authController = require("../controllers/auth");
 const router = express.Router();
 
-router
-  .route("/booking")
-  .post(authController.isAuthenticated, bookingController.getMyBookings);
+router.use(authController.isAuthenticated);
+
+router.route("/booking").post(bookingController.getMyBookings);
 
 router
   .route("/booking/:id")
-  .delete(authController.isAuthenticated, bookingController.cancelBooking)
-  .patch(authController.isAuthenticated, bookingController.editBooking);
-router
-  .route("/hotels/:hotelId/booking")
-  .post(authController.isAuthenticated, bookingController.createBooking);
+  .delete(bookingController.cancelBooking)
+  .patch(bookingController.editBooking);
+router.route("/hotels/:hotelId/booking").post(bookingController.createBooking);
 module.exports = router;
